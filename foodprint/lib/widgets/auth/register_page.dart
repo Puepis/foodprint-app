494cd8c2b90dbb67c2015ec067343c1bb49c7cbd
Philips Xu <philips.ilips@gmail.com>
2020-06-20 03:22:41 -0400

    styled register page

--- foodprint/lib/widgets/auth/register_page.dart
+++ foodprint/lib/widgets/auth/register_page.dart
@@ -34,14 +34,66 @@ class _RegisterPageState extends State<RegisterPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.white,
       body: SafeArea(
-        child: ListView(
-          padding: const EdgeInsets.symmetric(horizontal: 24.0),
-          children: <Widget>[
-            header,
-            registerForm(),
-          ],
-        ),
+          child: Container(
+            padding: const EdgeInsets.fromLTRB(25, 70, 25, 25),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Image.asset(
+                  'assets/images/logo.png',
+                  height: 75,
+                  width: 75,
+                ),
+                const Text(
+                  "Create an account",
+                  style: TextStyle(
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.w600
+                  ),
+                ),
+                const SizedBox(height: 2.5,),
+                const Text(
+                  "to begin tracking your foodprint",
+                  style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.grey
+                  ),
+                ),
+                const SizedBox(height: 60.0,),
+                registerForm(),
+                const SizedBox(height: 30.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      "Already have an account?",
+                      style: TextStyle(
+                        fontSize: 14.0,
+                        color: Colors.black,
+                      ),
+                    ),
+                    const SizedBox(width: 4.0,),
+                    InkWell(
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                      child: const Text(
+                        "Login",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.orange,
+                            fontSize: 14.0
+                        ),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          )
       ),
     );
   }
@@ -55,118 +107,109 @@ class _RegisterPageState extends State<RegisterPage> {
         ),
   );
 
-  Container header = Container(
-    padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 30.0),
-    child: const Text("Register New Account"),
-  );
-
-  Container registerForm() {
-    return Container(
-      padding: const EdgeInsets.symmetric(vertical: 20.0),
-      margin: const EdgeInsets.only(top: 30.0),
-      child: Form(
-        key: _formKey,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            TextFormField(
-              decoration: const InputDecoration(
-                  labelText: "Email"
+  Form registerForm() {
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          TextFormField(
+            decoration: InputDecoration(
+              labelText: "Email",
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(7.0)
               ),
-              onSaved: (String value) {
-                _email = value.trim();
-              },
-              validator: (String value) {
-                final RegExp re = RegExp(r"\S+@\S+\.\S+"); // matches anystring@anystring.anystring
-                if (value.isEmpty) {
-                  return 'Please enter an email';
-                }
-                else if (!re.hasMatch(value)) {
-                  return 'Please enter a valid email';
-                }
-                else {
-                  return null; // valid
-                }
+            ),
+            onSaved: (String value) {
+              _email = value.trim();
+            },
+            validator: (String value) {
+              final RegExp re = RegExp(r"\S+@\S+\.\S+"); // matches anystring@anystring.anystring
+              if (value.isEmpty) {
+                return 'Please enter an email';
               }
-            ),
-            const SizedBox(height: 12.0),
-            TextFormField(
-              decoration: const InputDecoration(
-                  labelText: "Username"
-              ),
-              onSaved: (String value) {
-                _username = value.trim();
-              },
-              validator: (String value) {
-                if (value.isEmpty) {
-                  return 'Please enter a username';
-                }
-                else if (value.length > 20) {
-                  return 'Username must not be longer than 20 characters';
-                }
-                else {
-                  return null;
-                }
+              else if (!re.hasMatch(value)) {
+                return 'Please enter a valid email';
               }
-            ),
-            const SizedBox(height: 12.0),
-            TextFormField(
-              decoration: const InputDecoration(
-                  labelText: "Password"
+              else {
+                return null; // valid
+              }
+            }
+          ),
+          const SizedBox(height: 20.0),
+          TextFormField(
+            decoration: InputDecoration(
+              labelText: "Username",
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(7.0)
               ),
-              onSaved: (String value) {
-                _password = value.trim();
-              },
-              validator: (String value) {
-                if (value.isEmpty) {
-                  return 'Please enter a password';
-                }
-                else if (value.length > 20) {
-                  return 'Password must not be longer than 20 characters';
-                }
-                else {
-                  return null;
-                }
-              },
-              obscureText: true,
             ),
-            buttons()
-          ],
-        ),
+            onSaved: (String value) {
+              _username = value.trim();
+            },
+            validator: (String value) {
+              if (value.isEmpty) {
+                return 'Please enter a username';
+              }
+              else if (value.length > 20) {
+                return 'Username must not be longer than 20 characters';
+              }
+              else {
+                return null;
+              }
+            }
+          ),
+          const SizedBox(height: 20,),
+          TextFormField(
+            decoration: InputDecoration(
+              labelText: "Password",
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(7.0)
+              ),
+            ),
+            onSaved: (String value) {
+              _password = value.trim();
+            },
+            validator: (String value) {
+              if (value.isEmpty) {
+                return 'Please enter a password';
+              }
+              else if (value.length > 20) {
+                return 'Password must not be longer than 20 characters';
+              }
+              else {
+                return null;
+              }
+            },
+          ),
+          const SizedBox(height: 30),
+          ButtonTheme(
+            height: 50,
+            child: RaisedButton(
+              color: Colors.orange,
+              shape: const RoundedRectangleBorder(
+                borderRadius:BorderRadius.all(Radius.circular(7.0)),
+              ),
+              onPressed:() async {
+                if (_formKey.currentState.validate()) {
+                  _formKey.currentState.save();
+                  _formKey.currentState.reset();
+                  final http.Response res = await AuthenticationService.attemptSignUp(_email, _username, _password);
+                  handleRegisterResponse(context, res);
+                }
+              },
+              child: const Text(
+                'Register',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 25
+                ),
+              ),
+            ),
+          )
+        ],
       ),
-    );
-  }
-
-  ButtonBar buttons() {
-    return ButtonBar(
-      children: <Widget>[
-        RaisedButton(
-            elevation: 3.0,
-            shape: const BeveledRectangleBorder(
-              borderRadius:BorderRadius.all(Radius.circular(7.0)),
-            ),
-            onPressed:() async {
-              Navigator.pop(context);
-            },
-            child: const Text('Back to Login Page'),
-        ),
-
-        RaisedButton(
-            elevation: 8.0,
-            shape: const BeveledRectangleBorder(
-              borderRadius:BorderRadius.all(Radius.circular(7.0)),
-            ),
-            onPressed:() async {
-              if (_formKey.currentState.validate()) {
-                _formKey.currentState.save();
-                _formKey.currentState.reset();
-                final http.Response res = await AuthenticationService.attemptSignUp(_email, _username, _password);
-                handleRegisterResponse(context, res);
-              }
-            },
-            child: const Text('Register'),
-        )
-      ],
     );
   }
 }
