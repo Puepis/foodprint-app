31e7b8ec05a30ba1162383464cc8f747e371ae41
Philips Xu <philips.ilips@gmail.com>
2020-07-20 00:24:30 -0400

    changed restaurant button border

--- foodprint/lib/presentation/camera_route/restaurants/select_restaurant_page.dart
+++ foodprint/lib/presentation/camera_route/restaurants/select_restaurant_page.dart
@@ -223,7 +223,7 @@ class _SelectRestaurantPageState extends State<SelectRestaurantPage> {
   Widget _buildButton(int index) {
     return Card(
       shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(15.0)
+        borderRadius: BorderRadius.circular(10.0)
       ),
       margin: const EdgeInsets.symmetric(vertical: 4.0),
       color: foodprintPrimaryColorSwatch[200],
