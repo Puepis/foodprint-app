e16c0df62baad1ce1b252ecc9444920a08af3f54
Philips Xu <philips.ilips@gmail.com>
2020-07-31 21:52:49 -0400

    Revert "used flutter_native_splash to generate native splash page"
    This reverts commit 9a36d66a290c570a99a6ca4c166f112a1bf20d6f.

--- foodprint/android/app/src/main/kotlin/com/foodprint/foodprint/MainActivity.kt
+++ foodprint/android/app/src/main/kotlin/com/foodprint/foodprint/MainActivity.kt
@@ -5,9 +5,6 @@ import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
 
-import android.os.Build
-import android.view.ViewTreeObserver
-import android.view.WindowManager
 class MainActivity: FlutterActivity() {
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         GeneratedPluginRegistrant.registerWith(flutterEngine);
